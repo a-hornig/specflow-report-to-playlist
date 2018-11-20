0459e12d7352404bce41c8527ce83cefd9103d84
ahornig <a.hornig.001@gmail.com>
2018-11-20 21:43:15 +1100

    Added initial project files and basic functionality.

---
+++ README.md
@@ -0,0 +1,1 @@
+# specflow-report-to-playlist
---
+++ SpecFlowReport2Playlist/StringExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System.Text.RegularExpressions;
+
+namespace System
+{
+    public static class StringExtensions
+    {
+        public static bool IsNullOrEmpty(this string text)
+        {
+            return string.IsNullOrEmpty(text);
+        }
+
+        public static string GetRegExMatch(this string text, string pattern)
+        {
+            var result = "";
+            var regex = new Regex(pattern, RegexOptions.Singleline);
+            var match = regex.Match(text);
+            if (match.Success)
+                result = match.Value;
+            return result;
+        }
+
+        public static string GetRegExCaptureGroup(this string text, string pattern, int groupIndex = 1)
+        {
+            var result = "";
+            var regex = new Regex(pattern, RegexOptions.Singleline);
+            var match = regex.Match(text);
+            if (match.Success)
+                result = match.Groups[groupIndex].Value;
+            return result;
+        }
+
+        public static string ReplaceRegExMatch(this string text, string pattern, string replacementText)
+        {
+            return Regex.Replace(text, pattern, replacementText, RegexOptions.Singleline);
+        }
+
+        public static string ReplaceRegExMatch(this string text, string pattern, MatchEvaluator replacementMethod)
+        {
+            return Regex.Replace(text, pattern, replacementMethod);
+        }
+
+    }
+}
